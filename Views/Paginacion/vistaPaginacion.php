fab5091ebf3ae651cf82e46f531efe767634a978
Hnry1304 <henryjvp24@hotmail.com>
2021-09-19 02:24:30 -0500

    Fin

--- Views/Paginacion/vistaPaginacion.php
+++ Views/Paginacion/vistaPaginacion.php
@@ -1,17 +1,23 @@
+<?php 
+    $function = $_SESSION['function'];
+    $product = $_SESSION['product'];
+    $class = 'class=ViewTienda';
+?>
+
 <div class="paginacion">
     <?php if($pagina == 0 || $pagina == 1):?>
-        <a href="?class=ViewTienda&function=<?= $_SESSION['function']?>&product=<?= $_SESSION['product'] ?>&pagina=<?= $pagina -1 ?>"class="disiable">Back</a>
+        <a href="?<?=$class?>&function=<?=$function?>&product=<?=$product?>&pagina=<?=$pagina -1 ?>"class="disiable">Back</a>
     <?php else:?>
-        <a href="?class=ViewTienda&function=<?= $_SESSION['function']?>&product=<?= $_SESSION['product'] ?>&pagina=<?= $pagina -1 ?>">Back</a>
+        <a href="?<?=$class?>&function=<?=$function?>&product=<?=$product?>&pagina=<?= $pagina -1 ?>">Back</a>
     <?php endif ?>
 
     <?php for($i=0; $i<$numero_paginacion; $i++): ?>
         <?php if($pagina == $i+1):?>
-            <a href="?class=ViewTienda&function=<?= $_SESSION['function']?>&product=<?= $_SESSION['product'] ?>&pagina=<?=$i+1 ?>" class="posicion">
+            <a href="?<?=$class?>&function=<?=$function?>&product=<?=$product?>&pagina=<?=$i+1 ?>" class="posicion">
                 <?= $i+1 ?>
             </a>
         <?php else: ?>
-            <a href="?class=ViewTienda&function=<?= $_SESSION['function']?>&product=<?= $_SESSION['product'] ?>&pagina=<?=$i+1 ?>">
+            <a href="?<?=$class?>&function=<?=$function?>&product=<?=$product?>&pagina=<?=$i+1 ?>">
                 <?= $i+1 ?>
             </a>
         <?php endif ?>
@@ -19,8 +25,8 @@
     <?php endfor ?>
 
     <?php if($pagina == $numero_paginacion):?>
-        <a href="?class=ViewTienda&function=<?= $_SESSION['function']?>&product=<?= $_SESSION['product'] ?>&pagina=<?=$pagina +1 ?>" class="disiable">Next</a>
+        <a href="?<?=$class?>&function=<?=$function?>&product=<?=$product?>&pagina=<?=$pagina +1 ?>" class="disiable">Next</a>
     <?php else:?>
-        <a href="?class=ViewTienda&function=<?= $_SESSION['function']?>&product=<?= $_SESSION['product'] ?>&pagina=<?=$pagina +1 ?>">Next</a>
+        <a href="?<?=$class?>&function=<?=$function?>&product=<?=$product?>&pagina=<?=$pagina +1 ?>">Next</a>
     <?php endif?>
 </div>
